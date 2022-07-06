fcdacfa2fbd0df74bb942ae15ad5a7a6c1913673
Mukku Suneel <108790373+sunil-goud@users.noreply.github.com>
2022-07-06 14:54:23 +0530

    Python code file

--- task.py
+++ task.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import datasets
 
+
 # Load the iris dataset
 iris = datasets.load_iris()
 iris_df = pd.DataFrame(iris.data, columns = iris.feature_names)
